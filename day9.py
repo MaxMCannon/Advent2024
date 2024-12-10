1b9a28b786af6b2d730c97ab289bcb0733a10e00
unknown <maxmcannon@gmail.com>
2024-12-10 17:22:07 -0500

    finished 9 p 1

--- day9.py
+++ day9.py
@@ -1,71 +1,62 @@
 #Advent of Code 2024
 #12/9
+import helpers as hp
 
 line = open('input.txt').read().splitlines()[0]
 
 # print(line)
 
-diskmap = ''
+diskmap = []
 
 file = True
 currID = 0
 
 for c in line:
     if file:
-        diskmap += str(currID) * int(c)
+        for i in range(int(c)):
+            diskmap.append(currID)
         currID += 1
         file = False
     else:
-        diskmap += '.' * int(c)
+        for i in range(int(c)):
+            diskmap.append('.')
         file = True
 
-print(diskmap)
-countnums = 0
+# print(diskmap)
+dm2 = hp.copylist(diskmap)
 
-for c in line:
-    if c.isnumeric():
-        countnums += 1
-
-def getlast():
-    global diskmap
-    last = len(diskmap)-1
-    while not diskmap[last].isnumeric():
-        last -= 1
-    out = diskmap[last]
-    # print(diskmap[last])
-    diskmap = diskmap[:last]
-    return out
-
+for i in range(len(diskmap)):
+    try:
+        if diskmap[i] == '.':
+            while diskmap[-1] == '.':
+                diskmap.pop()
+            diskmap[i] = diskmap[-1]
+            diskmap.pop()
+        # print(diskmap)
+    except:
+        print("done")
+        break
 
 checksum = 0
 for i in range(len(diskmap)):
-    if diskmap[i] == '.':
-        checksum += i * int(getlast())
-    else:
-        checksum += i * int(diskmap[i])
-    print(checksum)
+    checksum += i * diskmap[i]
 
+print(checksum)
 
-# def getlast():
-#     global diskmap
-#     out = ''
-#     for i in range(len(diskmap)-1, 0, -1):
-#         if diskmap[i] != '.':
-#             out = diskmap[i]
-#             # print(out)
-#             diskmap = diskmap[:i] + '.' + diskmap[i+1:]
-#             break
-#     return out
+print(dm2)
 
-# for i in range(countnums):
-#     if diskmap[i] == '.':
-#         diskmap = diskmap[:i] + getlast() + diskmap[i+1:]
-#         # print(diskmap)
+openspaces = []
+currID = dm2[-1]
 
-# checksum = 0
-# for i in range(countnums):
-#     checksum += i * int(diskmap[i])
+def getspace(i):
+    count = 0
+    while dm2[i] == '.':
+        count += 1
+        i += 1
+    print(count)
+    return count
 
-# print(checksum)
-
+it = 0
+while it < len(dm2):
+    if dm2[it] == '.':
         
